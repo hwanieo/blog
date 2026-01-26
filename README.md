11e6b9a2fb49f55823c3d30f8bd04c92d49d3b3f
Jonghwan Lee <hwniieo@gmail.com>
2026-01-26 15:41:15 +0900

    blog project init

---
+++ README.md
@@ -0,0 +1,9 @@
+# 블로그
+
+## 기술 스택
+
+- React
+- React Router
+- Shadcn
+- Axios
+- Supabase
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { Button } from "./components/ui";
+
+function App() {
+  return <Button>Click Me!</Button>;
+}
+
+export default App;
